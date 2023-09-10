36d9f41234d6c115dac6d7c1ab7785d7433fc866
Tyler Yan <117669511+TyYan03@users.noreply.github.com>
2023-09-10 16:16:37 -0400

    Create utils_tests.py

---
+++ utils_tests.py
@@ -0,0 +1,37 @@
+import utils
+
+def test_reversed():
+  flag = true
+  if utils.reversed(0) != 0:
+    flag = false
+  if utils.reversed(92381) != 18329:
+    flag = false
+  if utils.reversed(9) != 9:
+    flag = false
+  if utils.reversed(12345) != 54321:
+    flag = false
+  if utils.reversed(012) != 21:
+    flag = false
+
+  if flag:
+    print("5/5 Tests Successful")
+  else:
+    print("Failed")
+
+def test_formatted():
+  flag = true
+  if utils.reversed(8) != 1000, 10:
+    flag = false
+  if utils.reversed(1245) != 10011011101, 2335:
+    flag = false
+  if utils.reversed(2) != 10, 2:
+    flag = false
+  if utils.reversed(999) != 1111100111, 1747:
+    flag = false
+  if utils.reversed(012) != 1100, 14:
+    flag = false
+
+  if flag:
+    print("5/5 Tests Successful")
+  else:
+    print("Failed")
