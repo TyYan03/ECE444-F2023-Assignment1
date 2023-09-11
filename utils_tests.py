5e6581deae59b4ca180bb964b8d481cb6d9d0666
Tyler Yan <117669511+TyYan03@users.noreply.github.com>
2023-09-11 17:54:00 -0400

    Update utils_tests.py, passed all test cases and runs properly

--- utils_tests.py
+++ utils_tests.py
@@ -1,17 +1,17 @@
-import utils
+from utils import utils
 
 def test_reversed():
-  flag = true
+  flag = True
   if utils.reversed(0) != 0:
-    flag = false
+    flag = False
   if utils.reversed(92381) != 18329:
-    flag = false
+    flag = False
   if utils.reversed(9) != 9:
-    flag = false
-  if utils.reversed(12345) != 54321:
-    flag = false
-  if utils.reversed(012) != 21:
-    flag = false
+    flag = False
+  if utils.reversed(12345.4) != -1:
+    flag = False
+  if utils.reversed("12") != -1:
+    flag = False
 
   if flag:
     print("5/5 Tests Successful")
@@ -19,19 +19,22 @@ def test_reversed():
     print("Failed")
 
 def test_formatted():
-  flag = true
+  flag = True
   if utils.formatter(8) != (1000, 10):
-    flag = false
+    flag = False
   if utils.formatter(1245) != (10011011101, 2335):
-    flag = false
-  if utils.formatter(2) != (10, 2):
-    flag = false
+    flag = False
   if utils.formatter(999) != (1111100111, 1747):
-    flag = false
-  if utils.formatter(012) != (1100, 14):
-    flag = false
+    flag = False
+  if utils.formatter(2.3) != (-1, -1):
+    flag = False
+  if utils.formatter("12") != (-1, -1):
+    flag = False
 
   if flag:
     print("5/5 Tests Successful")
   else:
     print("Failed")
+
+test_reversed()
+test_formatted()
